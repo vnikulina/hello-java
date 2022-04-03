317db102ebb2c6aa369a98e3e29a2e5758abb3ed
vnikulina <valeriya.nikulina@gmail.com>
2022-04-03 17:33:53 +0300

    the second line contains another question

--- src/com/vnikulina/Main.java
+++ src/com/vnikulina/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world in java!");
+        System.out.println("What's going on today?!");
     }
 }
