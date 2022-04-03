5905862d6d0712168b5431c75573b0fddf546fd4
vnikulina <valeriya.nikulina@gmail.com>
2022-04-03 17:30:46 +0300

    a "beautiful day" line transformed to a question

--- src/com/vnikulina/Main.java
+++ src/com/vnikulina/Main.java
@@ -3,6 +3,7 @@ package com.vnikulina;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world in java!");
+        System.out.println("Hello world in Java!");
+        System.out.println("Is today a beautiful day?");
     }
 }
